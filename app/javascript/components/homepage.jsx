71b54e9fe445d45e433d0caa207da7e28bc4bdd1
paulderuelle <p.deruelle.mail@gmail.com>
2023-08-05 14:13:35 +0200

    full fix body/main/footer/waves

--- app/javascript/components/homepage.jsx
+++ app/javascript/components/homepage.jsx
@@ -1,10 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef } from 'react';
+import Typed from 'typed.js';
 
 function Homepage() {
+  const typedRef = useRef(null);
+
+  useEffect(() => {
+    const options = {
+      strings: [
+        `<p><span class="hightlight">/&gt;</span> Welcome to my “<span class="hightlight"><em>Crafted Realm</em></span>”!<br />
+        I'm <span class="hightlight">glad you're here</span> and hope you have a fullfiling day.</p>`
+      ],
+    typeSpeed: 25,
+    showCursor: false,
+  };
+
+  const typed = new Typed(typedRef.current, options);
+    return () => {
+      typed.destroy();
+    };
+  }, []);
 
   return (
     <>
-      <h1>Hello homepage!</h1>
+    <div class="maintitle">
+      <h1><span class="hightlight">/</span> PAUL DERUELLE<br />FULL-STACK<span class="hightlight pipe"> WEB DEVELOPER</span></h1>
+      <span ref={typedRef} />
+    </div>
     </>
   );
 }
--- app/javascript/components/main.jsx
+++ app/javascript/components/main.jsx
@@ -5,7 +5,7 @@ import Portfolio from './portfolio.jsx';
 import Contact from './contact.jsx';
 
 function Main() {
-  const [activeSection, setActiveSection] = useState(null);
+  const [activeSection, setActiveSection] = useState('homepage');
 
   const handleShowSection = (section) => {
     setActiveSection(section);
@@ -37,7 +37,7 @@ function Main() {
           {activeSection === 'contact' && <Contact />}
         </main>
 
-      <svg className="waves" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shapeRendering="auto">
+        <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shapeRendering="auto">
         <defs>
           <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
         </defs>
@@ -48,25 +48,25 @@ function Main() {
           <use xlinkHref="#gentle-wave" x="48" y="1" fill="rgba(255, 255, 255, 0.33)" />
         </g>
       </svg>
-      <div className="footer">
-        <div className="content">
-          <p>_CONNECT\WITH/ME_</p>
-          <p>
-            <a href="https://github.com/paulderuelle" target="_blank" rel="noopener noreferrer">
-              <i className="fab fa-github"></i>
-            </a>
-            <a href="https://www.linkedin.com/in/paulderuelle/" target="_blank" rel="noopener noreferrer">
-              <i className="fab fa-linkedin-in"></i>
-            </a>
-            <a href="https://www.instagram.com/scribolux/" target="_blank" rel="noopener noreferrer">
-              <i className="fab fa-instagram"></i>
-            </a>
-            <a href="https://www.facebook.com/paul.deruelle.reims/" target="_blank" rel="noopener noreferrer">
-              <i className="fab fa-facebook-f"></i>
-            </a>
-          </p>
-        </div>
+
+      <div className="footer-content">
+        <p>_CONNECT\WITH/ME_</p>
+        <p>
+          <a href="https://github.com/paulderuelle" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-github"></i>
+          </a>
+          <a href="https://www.linkedin.com/in/paulderuelle/" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-linkedin-in"></i>
+          </a>
+          <a href="https://www.instagram.com/scribolux/" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-instagram"></i>
+          </a>
+          <a href="https://www.facebook.com/paul.deruelle.reims/" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-facebook-f"></i>
+          </a>
+        </p>
       </div>
+
     </>
   );
 }
