8bedaa802a6cd5ffc1c4cf801f61ed112143cf69
paulderuelle <p.deruelle.mail@gmail.com>
2023-08-16 15:35:41 +0200

    corrected routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   namespace :api do
-    post 'send-email', to: 'email#send_email'
+    post 'send-email', to: 'contact#send_email'
   end
 end
