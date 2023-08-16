ff58b56b8ad5df6cfd66e3b95f26d5fa7f94190e
paulderuelle <p.deruelle.mail@gmail.com>
2023-08-16 17:19:16 +0200

    testing

--- app/controllers/api/contact_controller.rb
+++ app/controllers/api/contact_controller.rb
@@ -4,6 +4,10 @@ class Api::ContactController < ApplicationController
     email = params[:email]
     message = params[:message]
 
+    Rails.logger.info("Fullname: #{fullname}")
+    Rails.logger.info("Email: #{email}")
+    Rails.logger.info("Message: #{message}")
+
     ContactMailer.send_email(fullname, email, message).deliver_now
     render json: { message: 'Email sent successfully' }
   end
